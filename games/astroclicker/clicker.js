8315cff0caf0d29b98dbc0f3de06ccf5e545a4a0
Joe-vis <joel.visakorpi@gmail.com>
2024-09-25 01:24:57 +0300

    yf

--- games/astroclicker/clicker.js
+++ games/astroclicker/clicker.js
@@ -15,27 +15,30 @@
     }
 
 
+    Array.prototype.forEach.call(document.getElementsByClassName("shoptab"), element => {
+        element.style.display = "none"
+        console.log(element.id)
+        
+    });
+
     switch(name){
         default:
         case "utab":
             document.getElementById("pointshop").style.backgroundColor = "#2D2D2D"
-            document.getElementsByClassName("shoptab")[2].style.display = "none"
-            document.getElementsByClassName("shoptab")[1].style.display = "none"
             document.getElementsByClassName("shoptab")[0].style.display = "block"
             break;
 
         case "ctab":
             document.getElementById("pointshop").style.backgroundColor = "gold"
-            document.getElementsByClassName("shoptab")[2].style.display = "none"
             document.getElementsByClassName("shoptab")[1].style.display = "block"
-            document.getElementsByClassName("shoptab")[0].style.display = "none"
             break;
 
         case "dtab":
-            document.getElementById("pointshop").style.backgroundColor = "#860111"
+            document.getElementById("pointshop").style.backgroundColor = "#722aa1"
             document.getElementsByClassName("shoptab")[2].style.display = "block"
-            document.getElementsByClassName("shoptab")[1].style.display = "none"
-            document.getElementsByClassName("shoptab")[0].style.display = "none"
+
+            break;
+        case "shiptab":
             break;
 
     }
@@ -77,6 +80,7 @@
 
 (function(){
 
+    var meteorHealth = 100
     var points = 0;
     var multiplier = 1;
     var click = 1;
@@ -84,7 +88,7 @@
     var timeout;
     var value = document.getElementById("cost");
     var msg = document.getElementById("message");
-    var merchanttxt = ["You need points for that", "You can't afford that", "You need more points"];
+    var merchanttxt = ["You need credits for that", "You can't afford that", "Try again when you have some creds"];
     var spiritualisttxt = ["DON'T TOUCH THAT", "Careful with that", "That could curse you"];
 
     var thebook = false;
@@ -100,7 +104,6 @@
 
 
     // Heads
-
     var ownskimihead = false;
     var ownskrohead = false;
     var ownsostrighead = false;
@@ -108,82 +111,42 @@
 
 
     // MP change
-
     document.getElementsByClassName("btn")[0].onclick = function() {mpchange()};
     document.getElementsByClassName("btn")[1].onclick = function() {mpchange(1)};
 
 
-
-
-
     // On click
-
     document.getElementById("click").onclick = function() {pointup()};
-
     document.getElementById("mup").onclick = function() {multup(mp), cost(1)};
-
     document.getElementById("cup").onclick = function() {clickup(mp), cost(2)};
-
     document.getElementById("kro").onclick = function() {kro(), cost(4)};
-
     document.getElementById("ostrig").onclick = function() {ostrig(), cost(5)};
-
     document.getElementById("price").onclick = function() {thegoldenclock()};
-
     document.getElementById("robot").onclick = function() {activaterobot()};
-
     document.getElementById("robotparts").onclick = function() {robotparts(mp), cost(8)};
-
     document.getElementById("BOS").onclick = function() {bookofSacrifice()};
 
 
-
-
-
-
-
     // On Hover
-
     document.getElementById("mup").onmouseover  = function() {cost(1)};
-
     document.getElementById("cup").onmouseover  = function() {cost(2)};
-
-    document.getElementById("kro").onmouseover  = function() {cost(4)};
-
-    document.getElementById("ostrig").onmouseover  = function() {cost(5)};
-
     document.getElementById("price").onmouseover  = function() {cost(6)};
-
     document.getElementById("robot").onmouseover = function() {cost(7)};
-
     document.getElementById("robotparts").onmouseover = function() {cost(8)};
-
     document.getElementById("BOS").onmouseover = function() {cost(9)};
 
-
-
-
-
+    // Cosmetic
+    document.getElementById("kro").onmouseover  = function() {cost(4)};
+    document.getElementById("ostrig").onmouseover  = function() {cost(5)};
     // On Hover Exit
-
     document.getElementById("mup").onmouseleave = function(){cost()};
-
     document.getElementById("cup").onmouseleave  = function() {cost()};
-
     document.getElementById("kro").onmouseleave  = function() {cost()};
-
     document.getElementById("ostrig").onmouseleave  = function() {cost()};
-
     document.getElementById("price").onmouseleave  = function() {cost()};
-
     document.getElementById("robot").onmouseleave = function() {cost()};
-
     document.getElementById("robotparts").onmouseleave = function() {cost()};
-
     document.getElementById("BOS").onmouseleave = function() {cost()};
-
-
-
 
 
     function mpchange(x) {
@@ -198,110 +161,54 @@
         }
     }
 
-
-
-
-
     function cost(item) {
-
+        value.style.padding= "12px 16px 12px 16px"
         switch(item){
-
             default:
-            value.style.padding= "0"
-
-            value.innerHTML = "";
-
-            break;
-
+                value.style.padding= "0"
+                value.innerHTML = "";
+                break;
             case 1:
-
-            value.style.padding= "12px 16px 12px 16px"
-
-            if(multiplier >= 80){
-
-            value.innerHTML = "Already at max";
-
-            } else { value.innerHTML = "cost: " + ~~(100 * (1.15 ** multiplier * multiplier)); }
-
-            console.log(~~(100 * (1.05 * multiplier)))
-
-            break;
-
+                if(multiplier >= 80){
+                    value.innerHTML = "Already at max";
+                } else { value.innerHTML = "cost: " + ~~(100 * (1.15 ** multiplier * multiplier)); }
+                console.log(~~(100 * (1.05 * multiplier)))
+                break;
             case 2:
-
-            if(click >= 50){
-
-            value.innerHTML = "Already at max";
-
-            } else{ value.innerHTML = "cost: " + ~~(200 * (1.25 ** click * click)); }
-
-            value.style.padding= "12px 16px 12px 16px"
-
-            break;
+                if(click >= 50){
+                    value.innerHTML = "Already at max";
+                } else{ value.innerHTML = "cost: " + ~~(200 * (1.25 ** click * click)); }
+                break;
 
             case 3:
-
-            value.style.padding= "12px 16px 12px 16px"
-
-            value.innerHTML = "cost: " + 500;
-
-            break;
+                value.innerHTML = "cost: " + 500;
+                break;
 
             case 4:
-
-            value.style.padding= "12px 16px 12px 16px"
-
-            value.innerHTML = "cost: " + 10000;
-
-            break;
+                value.innerHTML = "cost: " + 10000;
+                break;
 
             case 5:
-
-            value.style.padding= "12px 16px 12px 16px"
-
-            value.innerHTML = "cost: " + 1000000;
-
-            break;
+                value.innerHTML = "cost: " + 1000000;
+                break;
 
             case 6:
-
-            value.style.padding= "12px 16px 12px 16px"
-
-            value.innerHTML = "cost: " + "Y̴̡̧̩̜̼̠̝̙͇͇̦͚̯̰̦͇̣̓̐͊ō̷̭̹͂̉̇ȕ̴͎̰̭̫̐̄̏͒̎̀̕͜r̴͓̘̺̄̐̈́̔̀̋̾̍̈́́́͒̐͒ ̶̡̢͇̬͕̘̫̩͈̣͖͎͖͖̙͗́̽̾l̸̦̣͈͎͕̭̟̅͛̽͛̆̆́͊̈́͗̌i̴̡̱̗̟̣̥̠̠͕͐̉̾f̴̺̼̜̭̲̺̺̻̞͊̿ͅë̶̤́́̎̄̑̽́̽͛̌̍̑͐̃͝͝͝";
-
-            break;
+                value.innerHTML = "cost: " + "Y̴̡̧̩̜̼̠̝̙͇͇̦͚̯̰̦͇̣̓̐͊ō̷̭̹͂̉̇ȕ̴͎̰̭̫̐̄̏͒̎̀̕͜r̴͓̘̺̄̐̈́̔̀̋̾̍̈́́́͒̐͒ ̶̡̢͇̬͕̘̫̩͈̣͖͎͖͖̙͗́̽̾l̸̦̣͈͎͕̭̟̅͛̽͛̆̆́͊̈́͗̌i̴̡̱̗̟̣̥̠̠͕͐̉̾f̴̺̼̜̭̲̺̺̻̞͊̿ͅë̶̤́́̎̄̑̽́̽͛̌̍̑͐̃͝͝͝";
+                break;
 
             case 7:
-
-            value.style.padding= "12px 16px 12px 16px"
-
-            value.innerHTML = "cost: " + 1000;
-
-            break;
+                value.innerHTML = "cost: " + 1000;
+                break;
 
             case 8:
-
-            value.style.padding= "12px 16px 12px 16px"
-
-            if(passiveClick >= 70){
-
-            value.innerHTML = "Already at max";
-
-            } else { value.innerHTML = "cost: " + partscost; }
-
-
-
-
-
-            break;
+                if(passiveClick >= 70){
+                value.innerHTML = "Already at max";
+                } else { value.innerHTML = "cost: " + partscost; }
+                break;
 
             case 9:
-
-            value.style.padding= "12px 16px 12px 16px"
-
-            value.innerHTML = "Inspect the book";
-
-            break;
+                value.innerHTML = "Inspect the book";
+                break;
 
         }
 
@@ -312,101 +219,59 @@
         switch(option) {
 
             case 1:
-
                 clearTimeout(timeout);
-
                 msg.innerHTML = "Merchant: " + merchanttxt[~~(Math.random() * 3)];
-
                 msg.style.opacity = 1;
-
                 timeout = setTimeout(function(){  msg.style.opacity = 0; }, 6000);
-
                 break;
 
             case 2:
-
                 clearTimeout(timeout);
-
                 msg.style.opacity = 1;
-
                 timeout = setTimeout(function(){  msg.style.opacity = 0; }, 6000);
-
                 break;
 
             case 3:
-
                 clearTimeout(timeout);
-
                 msg.innerHTML = "Merchant: Thats not for sale";
-
                 msg.style.opacity = 1;
-
                 timeout = setTimeout(function(){  msg.style.opacity = 0; }, 6000);
-
                 break;
 
             case 4:
-
                 clearTimeout(timeout);
-
                 msg.innerHTML = "Merchant: Sorry can't sell you that";
-
                 msg.style.opacity = 1;
-
                 timeout = setTimeout(function(){  msg.style.opacity = 0; }, 6000);
-
                 break;
 
            case 5:
-
                 clearTimeout(timeout);
-
                 msg.innerHTML = "Merchant: Just remember to recharge it";
-
                 msg.style.opacity = 1;
-
                 timeout = setTimeout(function(){  msg.style.opacity = 0; }, 6000);
-
                 break;
 
             case 6:
-
                 clearTimeout(timeout);
-
                 msg.innerHTML = "Merchant: A strange looking book, isn't it";
-
                 msg.style.opacity = 1;
-
                 timeout = setTimeout(function(){  msg.style.opacity = 0; }, 6000);
-
                 break;
 
             case 7:
-
                 clearTimeout(timeout);
-
                 msg.innerHTML = "I need the robot first";
-
                 msg.style.opacity = 1;
-
                 timeout = setTimeout(function(){  msg.style.opacity = 0; }, 6000);
-
                 break;
 
             case 8:
-
                 clearTimeout(timeout);
-
                 msg.innerHTML = "The robot is complete";
-
                 msg.style.opacity = 1;
-
                 timeout = setTimeout(function(){  msg.style.opacity = 0; }, 6000);
-
                 break;
-
-
-
         }
 
         }
@@ -422,88 +287,45 @@
         msg.style.opacity = 1;
 
         timeout = setTimeout(function(){  msg.style.opacity = 0; }, 6000);
-
-
-
             switch(option) {
-
                 default:
-
                 case 1:
-
                     msg.innerHTML = "Spiritualist: " + spiritualisttxt[~~(Math.random() * 3)];
-
                     break;
-
-
-
             }
-
-            }
-
-
-
-
-    function updatePoints() {
-
-        points = (Math.round((points + Number.EPSILON) * 10) / 10)
-
-        document.getElementById("points").innerHTML = "points: " + points.toLocaleString("en-US");
-
-    }
-
-
-
-    function pointup() {
-
-        if(multiplier > 80){multiplier = 80;}
-
-        if(click > 50){click = 50;}
-
-        points += click * multiplier;
-
-        updatePoints()
-
-
-
-    }
-
-
-
-    function multup(mp) {
-
-
-
-        for (i = 1; i <= mp; i++) {
-
-            if (multiplier <= 79){
-
-            if(points >= 100 * (1.15 ** multiplier * multiplier)) {
-
-
-
-                points -= ~~(100 * (1.15 ** multiplier * multiplier));
-
-                multiplier += .2
-
-                document.getElementById("mup").innerHTML = "multiplier: " + Math.round((multiplier + Number.EPSILON) * 100) / 100;
-
-                updatePoints()
-
-            }
-
-            else {merchantstext(1); break;}
-
-            } else {break;}
 
         }
 
 
+    function updatePoints() {
+        points = (Math.round((points + Number.EPSILON) * 10) / 10)
+        document.getElementById("points").innerHTML = "Credits: " + points.toLocaleString("en-US");
+
     }
 
+    function pointup() {
+        if(multiplier > 80){multiplier = 80;}
+        if(click > 50){click = 50;}
+        points += click * multiplier;
+        meteorHealth -= 10
+        document.getElementById("meteor_health").value = meteorHealth
+        updatePoints()
+    }
 
+    function multup(mp) {
+        for (i = 1; i <= mp; i++) {
 
-
+            if (multiplier <= 79){
+                if(points >= 100 * (1.15 ** multiplier * multiplier)) {
+                    points -= ~~(100 * (1.15 ** multiplier * multiplier));
+                    multiplier += .2
+                    document.getElementById("mup").innerHTML = "multiplier: " + Math.round((multiplier + Number.EPSILON) * 100) / 100;
+                    updatePoints()
+                }
+            else {merchantstext(1); break;}
+            } else {break;}
+        }
+    }
 
     function clickup(mp) {
 
@@ -635,8 +457,6 @@
         } else {spiritualisttext(1)}
 
     }
-
-
 
     function bookofSacrifice(){
         console.log("book")
